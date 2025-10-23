e1b016a1ac6a25d91b7421324b70153cfed3a8f9
qusnuldiah <qusnuldiah25@gmail.com>
2025-10-23 14:19:25 +0700

    UTS: Selesai Soal 1 - ListTile dan Navigasi Balik

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'home_screen.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   final String ocrText;
-
   const ResultScreen({super.key, required this.ocrText});
+
+  @override
+  State<ResultScreen> createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterTts = FlutterTts();
+    flutterTts.setLanguage('id-ID'); // Bahasa Indonesia
+  }
+
+  Future<void> speakText() async {
+    if (widget.ocrText.isNotEmpty) {
+      await flutterTts.speak(widget.ocrText);
+    }
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +40,34 @@ class ResultScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: SelectableText(
-            ocrText.isEmpty
+            widget.ocrText.isEmpty
                 ? 'Tidak ada teks ditemukan.'
-                : ocrText.replaceAll('\n', ' '),
+                : widget.ocrText,
             style: const TextStyle(fontSize: 18),
           ),
         ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'tts',
+            child: const Icon(Icons.volume_up),
+            onPressed: speakText,
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            heroTag: 'home',
+            child: const Icon(Icons.home),
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+                (route) => false,
+              );
+            },
+          ),
+        ],
       ),
     );
   }
